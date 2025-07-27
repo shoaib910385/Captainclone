d3d8cc02484dd81e925aa869b8b84227249499ce
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-27 11:24:48 +0530

    Update help.py

--- Clonify/cplugin/help.py
+++ Clonify/cplugin/help.py
@@ -107,7 +107,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                "😎 Pehle Zeo Ko Papa Bol Jake 😆😆", show_alert=True
+                "😎 Pehle Papa Bol  😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(helpers.HELP_9, reply_markup=keyboard)
