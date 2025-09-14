d0dee4fbcb907e3fa57e7124745158f4ea261d98
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-14 19:57:45 +0530

    Update start.py

--- Clonify/plugins/bot/start.py
+++ Clonify/plugins/bot/start.py
@@ -118,7 +118,7 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_photo(
+    await message.reply_video(
         random.choice(STREAMI_PICS),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
